ace09c7f0f49a3885246484c093848c2fd67581b
Aamir Ahmad <amirahmad@Aamirs-MacBook-Pro.local>
2017-09-24 06:47:17 +0530

    Changes

---
+++ manifests/params.pp
@@ -0,0 +1,8 @@
+class apache::params {
+	if $::osfamily == 'RedHat' {
+	$apachename = 'httpd'
+	} elseif $::osfamily = 'Debian' {
+	$apachename = 'apache2'
+	} else { 
+	print "This is not a supported distribution"}
+}
